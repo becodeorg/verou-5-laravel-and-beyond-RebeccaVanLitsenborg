e27c506b7aad9fee2d945ab8da05ad744fcf4ac9
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-04-12 11:23:18 +0200

    Set basic styling to register and login forms so that they are the same

--- resources/views/sessions/login.blade.php
+++ resources/views/sessions/login.blade.php
@@ -5,24 +5,32 @@
 @section('content')
 
 <section class="login">
-              <h1>
-                  Sign In
-              </h1>
-              <form method="post" action="{{ route('handleLogin') }}">
-              @csrf
-                  <div>
-                      <label for="email">Your email</label>
-                      <input type="email" name="email" id="email" placeholder="name@company.com" required="">
-                  </div>
-                  <div>
-                      <label for="password">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" required="">
-                  </div>
-                  <button type="submit">Sign In</button>
-                  <p>
-                      Don't yet have an account? <a href="/register">Register Here</a>
-                  </p>
-              </form>
+    <div class="login-welcome">
+    <div class="welcome-content">
+            <h1>Welcome</h1>
+        </div>
+    </div>
+
+        <form class="login-form" method="post" action="{{ route('handleLogin') }}">
+            @csrf
+            <h1 class="">
+                Sign in
+            </h1>
+            <div>
+                <label for="email" class="register-label">Your email</label>
+                <input type="email" name="email" id="email" class="" placeholder="name@company.com" required="">
+            </div>
+            <div>
+                <label for="password" class="register-label">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" class="" required="">
+            </div>
+            <button type="submit" class="create-account">Sign In</button>
+            <div class="login-link">
+                <p>
+                    Don't yet have an account? <a href="/register" class="link"><strong>Register Here</strong></a>
+                </p>
+            </div>
+        </form>
 </section>
 
 @endsection
--- resources/views/sessions/register.blade.php
+++ resources/views/sessions/register.blade.php
@@ -6,36 +6,38 @@
 
 <section class="register-account">
     <div class="register-welcome">
-        <h1>Welcome</h1>
-</div>
-              <form class="register-form" method="post" action="{{ route('submitRegister') }}">
-              @csrf
-              <h1 class="">
-                  Create an account
-              </h1>
-                  <div>
-                      <label for="email" class="register-label">Name</label>
-                      <input type="name" name="name" id="name" class="" placeholder="Your name" required="">
-                  </div>
-                  <div>
-                      <label for="email" class="register-label">Your email</label>
-                      <input type="email" name="email" id="email" class="" placeholder="name@company.com" required="">
-                  </div>
-                  <div>
-                      <label for="password" class="register-label">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="" required="">
-                  </div>
-                  <div>
-                      <label for="confirm-password" class="register-label">Confirm password</label>
-                      <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="" required="">
-                  </div>
-                  <button type="submit" class="create-account">Create an account</button>
-                  <div class="login-link">
-                  <p>
-                      Already have an account? <a href="/login" class="link"><strong>Login here</strong></a>
-                  </p>
-                  </div>
-              </form>
+    <div class="welcome-content">
+            <h1>Welcome</h1>
+        </div>
+    </div>
+            <form class="register-form" method="post" action="{{ route('submitRegister') }}">
+            @csrf
+            <h1 class="">
+                Create an account
+            </h1>
+                <div>
+                    <label for="email" class="register-label">Name</label>
+                    <input type="name" name="name" id="name" class="" placeholder="Your name" required="">
+                </div>
+                <div>
+                    <label for="email" class="register-label">Your email</label>
+                    <input type="email" name="email" id="email" class="" placeholder="name@company.com" required="">
+                </div>
+                <div>
+                    <label for="password" class="register-label">Password</label>
+                    <input type="password" name="password" id="password" placeholder="••••••••" class="" required="">
+                </div>
+                <div>
+                    <label for="confirm-password" class="register-label">Confirm password</label>
+                    <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="" required="">
+                </div>
+                <button type="submit" class="create-account">Create an account</button>
+                <div class="login-link">
+                <p>
+                    Already have an account? <a href="/login" class="link"><strong>Login here</strong></a>
+                </p>
+                </div>
+            </form>
 </section>
 
 @endsection
